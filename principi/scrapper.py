02ab107ddd0c61e3af842fe93ab6ad4228f47677
Hexxit6 <38117389+Hexxit6@users.noreply.github.com>
2022-05-23 23:46:39 +0200

    Add send function to scrapper

--- principi/scrapper.py
+++ principi/scrapper.py
@@ -1,7 +1,7 @@
 #!/usr/share/env python3
 
 # TODO:
-# [-] add function for sending data to the database (POST data to our API)
+# [+] add function for sending data to the database (POST data to our API)
 # [-] add error handling
 
 import requests
@@ -34,7 +34,32 @@ class Allergen:
 
 # Know when to use it and don't abuse it!
 def toJSON(obj):
-    return json.dumps(obj.__dict__)
+    data = {
+        "pollutants": {
+            "AQI": obj[3],
+            "PM2_5": obj[4][0].value,
+            "PM10": obj[4][1].value,
+            "NO2": obj[4][2].value
+        },
+        "allergens": {
+            "Birch": obj[5][0].value,
+            "Alder": obj[5][1].value,
+            "Grasses": obj[5][2].value,
+            "Ragweed": obj[5][3].value,
+            "Mugwort": obj[5][4].value,
+            "OliveTree": obj[5][5].value
+        } 
+    }
+    return json.dumps(data)
+    # return data
+
+
+def send(data):
+    url = "http://localhost:3000/data/"
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
+    res = requests.post(url, data=data, headers=headers)
+    if(ARGS.verbose):
+        print(res.text)     # VERBOSE/DEBUGGING
 
 
 def getStationInfo(source: str):
@@ -50,30 +75,35 @@ def getStationInfo(source: str):
         unit = item.find("div", class_="unit").text
         value = item.find("div", class_="value").text
         pollutantList.append(Pollutant(name, unit, value))
-    # print(pollutantList)    # DEBUGGING
+    if(ARGS.verbose):
+        print(pollutantList)    # VERBOSE/DEBUGGING
 
     allergenList = []
     for item in allergens.find_all("div", class_="pollutant-item"):
         name = item.find("div", class_="name").text
         value = item.find("div", class_="value").text
         allergenList.append(Allergen(name, value))
-    # print(allergenList)    # DEBUGGING
+    if(ARGS.verbose):
+        print(allergenList)    # VERBOSE/DEBUGGING
 
     # UPDATE TIME
     updateTime = soup.find("div", class_="update-time").text
-    # print(f"Update-time: {updateTime}") # DEBUGGING
+    if(ARGS.verbose):
+        print(f"Update-time: {updateTime}") # VERBOSE/DEBUGGING
 
     # INDEX VALUE (AQI)
     comments = soup.find_all(text=lambda text: isinstance(text, Comment))
     comments = ''.join(comments)
     indexValue = re.search('<div class="indexValue">(\d+)<\/div>', comments).groups()[0]
-    # print(f"Index value: {indexValue}") # DEBUGGING
+    if(ARGS.verbose):
+        print(f"Index value: {indexValue}") # VERBOSE/DEBUGGING
 
     # TITLE
     detail = soup.find("div", class_="detail-title")
     title = detail.h2.text
     location = detail.p.text
-    # print(f"Title: {title}, location: {location}")  # DEBUGGING
+    if(ARGS.verbose):
+        print(f"Title: {title}, location: {location}")  # VERBOSE/DEBUGGING
 
     return (title, location, updateTime, indexValue, pollutantList, allergenList)
 
@@ -89,7 +119,6 @@ def parseStations(filename):
     return stations
 
 
-# maybe have a default -s -i and -u settings?
 def arguments():
     parser = argparse.ArgumentParser(
         description="Air-quality scrapper for pollutant and allergen information.",
@@ -116,10 +145,11 @@ def main():
         case "days":
             INTERVAL = ARGS.interval * 86400
 
-    # while True:
-    #     sleep(INTERVAL)
-    #     for s in stations:
-    #         print(getStationInfo(s.url))
+    while True:
+        for s in stations:
+            data = getStationInfo(s.url)
+            send(toJSON(data))
+        sleep(INTERVAL)
 
 
 if __name__ == "__main__":
