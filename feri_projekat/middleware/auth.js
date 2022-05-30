af1bbe843d012ee65b6b17a391bd1ea156c837e5
Hexxit6 <38117389+Hexxit6@users.noreply.github.com>
2022-05-30 18:52:12 +0200

    Auth and minor fixes

--- feri_projekat/middleware/auth.js
+++ feri_projekat/middleware/auth.js
@@ -1,18 +1,70 @@
 const jwt = require("jsonwebtoken");
+var UserModel = require('../models/userModel.js');
 
-module.exports = function(req, res, next){
-    try {
-        // var token = req.cookies['jwt'];
-		const authHeader = req.headers.authorization;
-		const token = authHeader && authHeader.split(' ')[1];
-        if (token === 'undefined') return res.status(403).send("Access denied.");
-        var decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-		// req.userId = decoded.id;
-        // req.username = decoded.username;
-		req.session.userId = decoded.id;
-        req.session.username = decoded.username;
-        next();
-    } catch (error) {
-        res.status(400).send("Invalid token!");
-    }
-};
+module.exports = {
+	auth: function(req, res, next){
+		try {
+			if(req.session && req.session.userId){
+				return next();
+			} else {
+				var err = new Error("You must be logged in to view this page!");
+				err.status = 401;
+				return next(err);
+			}
+		} catch (error) {
+			return next(error);
+		}
+	},
+
+	isAdmin: function(req, res, next){
+		// console.log(req.session.userId);
+		try {
+			if(req.session && req.session.userId){
+				UserModel.findOne({_id: req.session.userId}, function(err, user){
+					if (err) {
+						return res.status(500).json({
+							message: 'Error when getting user.',
+							error: err
+						});
+					}
+
+					if (!user) {
+						return res.status(404).json({
+							message: 'No such user'
+						});
+					}
+
+					if (user.admin !== true) {
+						return res.status(401).json({
+							message: 'You are not authorized to view this page'
+						});
+					}
+
+					return next();
+				})
+			} else {
+				var err = new Error("You are not authorized to view this page!");
+				err.status = 401;
+				return next(err);
+			}
+		} catch (error) {
+			error.status = 401;
+			return next(error);
+		}
+	},
+
+	jwtPass: function(req, res, next){
+		try {
+			const authHeader = req.headers.authorization;
+			const token = authHeader && authHeader.split(' ')[1];
+			if (token === 'undefined') return res.status(403).send("Access denied.");
+			var decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+			req.session.userId = decoded.id;
+			req.session.username = decoded.username;
+			req.session.admin = decoded.admin;
+			next();
+		} catch(error) {
+			next();
+		}
+	},
+}
--- feri_projekat/routes/adminRoutes.js
+++ feri_projekat/routes/adminRoutes.js
@@ -2,20 +2,21 @@ var express = require('express');
 var router = express.Router();
 var adminController = require('../controllers/adminController.js');
 var settingsController = require('../controllers/settingsController.js');
+var auth = require('../middleware/auth.js');
 
 /*
  * GET
  */
-router.get('/', adminController.dashboard);
-router.get('/users', adminController.users);
-router.get('/settings', adminController.settings);
+router.get('/', auth.isAdmin ,adminController.dashboard);
+router.get('/users', auth.isAdmin, adminController.users);
+router.get('/settings', auth.isAdmin, adminController.settings);
 // router.get('/layout', adminController.dashboard);
 
 /*
  * POST
  */
-router.post('/settings/start', settingsController.start);
-router.post('/settings/stop', settingsController.stop);
+// router.post('/settings/start', auth.isAdmin, settingsController.start);
+// router.post('/settings/stop', auth.isAdmin, settingsController.stop);
 
 
 module.exports = router;
