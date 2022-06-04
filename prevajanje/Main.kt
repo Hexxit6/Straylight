4c4ebd5d998af0e7c180855dcd7fde02afcfd1b2
Hexxit6 <38117389+Hexxit6@users.noreply.github.com>
2022-06-04 22:25:41 +0200

    Add AST and parser (without named blocks)

--- prevajanje/Main.kt
+++ prevajanje/Main.kt
@@ -1,9 +1,36 @@
 fun main(args: Array<String>) {
-    val scanner = Scanner(Example, "city{let varijabla = 4.4+100;let varijabla = 4.4+100;Water{}}".byteInputStream())
+    // val scanner = Scanner(Example, "city{let varijabla = 4.4+100;let varijabla = 4.4+100;Water{}}".byteInputStream())
     // printTokens(scanner)
-	if (Recognizer(scanner).recognize()) {
-		println("accept")
-	} else {
-		println("reject")
-    }
+	// if (Recognizer(scanner).recognize()) {
+	// 	println("accept")
+	// } else {
+	// 	println("reject")
+    // }
+
+    // val expr: Expr = City(Line(Point(2.0,4.0), Point(3.0,6.0)))
+    // val expr: Expr = City(Box(Point(2.0,4.0), Point(3.0,6.0)))
+    // val expr: Expr = City()
+    val expr: Expr = City(
+        Comma(
+            Comma(
+                Line(
+                    Point(3.0,4.0),
+                    Point(3.0,6.0)
+                ),
+                Line(
+                    Point(2.0,4.0),
+                    Point(3.0,6.0)
+                )
+            ),
+            Line(
+                Point(2.0,4.0),
+                Point(3.0,4.0)
+            )
+        )
+    )
+    println(expr.toGeoJSON())
+
+    var scanner = Scanner(Example, "city{ Line(Point(3.0, 4.0), Point(3.0, 6.0)); Line(Point(2.0, 4.0), Point(3.0, 6.0)); Line(Point(2.0, 4.0), Point(3.0, 4.0)); }".byteInputStream())
+    val parser = Parser(scanner)
+    println(parser.parse())
 }
