4b17854f3e126bf14e3e938b87cec6f9761ecb8f
Lea_Tajnikar <lea.tajnikar@student.um.si>
2023-01-17 17:18:40 +0100

    Added: saving events with MQTT

--- pora_projekat/app/src/main/java/com/example/pora_projekat/MainActivity.kt
+++ pora_projekat/app/src/main/java/com/example/pora_projekat/MainActivity.kt
@@ -4,14 +4,17 @@ import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pora_projekat.databinding.ActivityMainBinding
 import com.example.pora_projekat.services.AudioService
 import com.example.pora_projekat.services.ImageService
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // TODO: delete
         // Example of starting the service
@@ -34,5 +37,11 @@ class MainActivity : AppCompatActivity() {
         //     intent.putExtra(AudioService.EXTRA_LONGITUDE, 0f)
         //     startService(intent)
         // }
+
+
+        binding.addEvent.setOnClickListener {
+            val intent = Intent(this, EventActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
